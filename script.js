2686534d71774d97475e5fc8d05104c3280da8f4
Ayon Pal <ayoooonpal@gmail.com>
2022-03-29 00:03:42 +0530

    update position of create

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 var index = 0;
 
 window.onload = document.querySelector("#user_input").select();
-
+var last_note_coord = [0,0];
 document.querySelector("#add_note").addEventListener("click", () => {
   document.querySelector("#pop_note").style.display = "block";
   document.querySelector("#user_input").value = '';
 });
 
- document.querySelector("#container_notes").addEventListener("click", () => {
+ document.querySelector("#container_notes").addEventListener("click", (e) => {
+   last_note_coord = [e.pageX, e.pageY];
   document.querySelector("#pop_note").style.display = "block";
   document.querySelector("#user_input").value = '';
 });
@@ -19,7 +20,7 @@ document.querySelector("#hide").addEventListener("click", () => {
 document.querySelector("#user_input").addEventListener('keydown', (event) => {
   if(event.key === 'Enter'){
     const text = document.querySelector("#user_input");
-    createStickyNote(text.value);
+    createStickyNote(text.value, last_note_coord);
     document.querySelector("#pop_note").style.display = "none";
   }
 });
@@ -30,7 +31,7 @@ document.querySelector("#user_input").addEventListener('keydown', (event) => {
   createStickyNote();
 }); */
 
-createStickyNote = (text) => {
+createStickyNote = (text, coord) => {
   var note = document.createElement("div");
   var details = document.createElement("div");
   var noteText = document.createElement("h1");
@@ -38,11 +39,11 @@ createStickyNote = (text) => {
   note.className = "note";
   details.className = "details";
   noteText.textContent = text;
-
+  console.log(coord);
   details.appendChild(noteText);
   note.appendChild(details);
 
-  note.setAttribute('style', 'width:280px; height:280px; background:linear-gradient(180deg, rgba(255,209,38,1) 0%, rgba(255,209,38,1) 54%, rgba(180,149,32,1) 100%);');
+  note.setAttribute('style', `width:280px; height:280px; background:linear-gradient(180deg, rgba(255,209,38,1) 0%, rgba(255,209,38,1) 54%, rgba(180,149,32,1) 100%); position:absolute; left: ${coord[0]}px; top:${coord[1]}px`);
 
   note.addEventListener("dblclick", () => {
     note.remove();
