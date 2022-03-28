7511fca4efc8ee30a82899bcf8aaf3907bb6660b
Max <max@rose-niestetal.de>
2022-03-28 15:13:20 +0200

    commit

---
+++ script.js
@@ -0,0 +1,39 @@
+var index = 0;
+
+window.onload = document.querySelector("#user_input").select();
+
+document.querySelector("#add_note").addEventListener("click", () => {
+  document.querySelector("#modal").style.display = "block";
+});
+
+document.querySelector("#hide").addEventListener("click", () => {
+  document.querySelector("#modal").style.display = "none";
+});
+
+document.querySelector("#user_input").addEventListener('keydown', (event) => {
+  if(event.key === 'Enter'){
+    const text = document.querySelector("#user_input");
+    createStickyNote(text.value);
+  }
+});
+
+createStickyNote = (text) => {
+  let note = document.createElement("div");
+  let details = document.createElement("div");
+  let noteText = document.createElement("h1");
+
+  note.className = "note";
+  details.className = "details";
+  noteText.textContent = text;
+
+  details.appendChild(noteText);
+  note.appendChild(details);
+
+  note.setAttribute('style', 'width:280px; height:280px; background:linear-gradient(180deg, rgba(255,209,38,1) 0%, rgba(255,209,38,1) 54%, rgba(180,149,32,1) 100%);');
+
+  note.addEventListener("dblclick", () => {
+    note.remove();
+  })
+
+  document.querySelector("#all_notes").appendChild(note);
+}
